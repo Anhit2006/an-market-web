5367fe4b7118963d08b44088f96cc2002547d55a
Anhit2006 <dangthaian6atp@gmail.com>
2025-06-25 22:56:04 +0700

    Cập nhật một số chức năng JavaScript mới

--- script.js
+++ script.js
@@ -153,7 +153,7 @@ if (form) {
   });
 }
 // Khi tải lại trang, luôn cuộn về đầu (top)
-window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
-};
+window.addEventListener("load", function () {
+    window.scrollTo(0, 0);
+});
 
